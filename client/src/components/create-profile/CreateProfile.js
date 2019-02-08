51a49b293ea8596343ad90d63712a11ee99de83f
Rom Vic <technoyguy@gmail.com>
2019-02-08 15:17:20 -0800

    added privateRoute

---
+++ client/src/components/create-profile/CreateProfile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import PropTypes from 'prop-types'
+
+class CreateProfile extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+export default connect()(CreateProfile)
